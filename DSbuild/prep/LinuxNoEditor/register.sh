2c0fe70afbba32fa7d489a7d242ec7cb86154b11
TeoFerdian <teoderikus@accelbyte.net>
2019-07-25 13:33:48 +0700

    fix(DS): removing start.sh, set register on different file.

---
+++ DSbuild/prep/LinuxNoEditor/register.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+BASE_URL="<INSERT-YOUR-BASE-URL-HERE>"
+BASIC_CREDS="<INSERT-YOUR-BASIC-CREDS-HERE>"
+namespace="<INSERT-YOUR-NAMESPACE-HERE>"
+
+CHECK=$(netstat -antp 2>/dev/null | grep $PORT | wc -l)
+
+echo $CHECK
+
+while [ "$CHECK" -eq "0" ]; do
+    CHECK=$(netstat -antp 2>/dev/null | grep $PORT | wc -l)
+    echo "Server is not ready yet.."
+    sleep 3
+done
+
+echo "Server is ready, sending register to DSM service.."
+
+response=$(curl -X POST --header "Content-Type: application/x-www-form-urlencoded" --header "Accept: application/json" --header "Authorization: Basic $BASIC_CREDS" -d "grant_type=client_credentials" "$BASE_URL/iam/oauth/token")
+token=$(echo "$response" | grep access_token | cut -d ':' -f 2 | cut -d '"' -f 2)
+curl -X POST --header "Content-Type: application/json" --header "Authorization: Bearer $token" -d "{ \"pod_name\": \"$POD_NAME\", \"port\": $PORT }" "$BASE_URL/dsm/namespaces/$namespace/servers/register"
--- DSbuild/prep/LinuxNoEditor/run.sh
+++ DSbuild/prep/LinuxNoEditor/run.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 UE4_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 UE4_PROJECT_ROOT=$(dirname "$UE4_TRUE_SCRIPT_NAME")
-BASE_URL="<INSERT-YOUR-BASE-URL-HERE>"
-BASIC_CREDS="<INSERT-YOUR-BASIC-CREDS-HERE>"
-namespace="<INSERT-YOUR-NAMESPACE-HERE>"
 
 CHECK="1"
 
@@ -13,24 +10,11 @@ while [ "$CHECK" -gt "0" ]; do
     CHECK=$(netstat -antp 2>/dev/null | grep $PORT | wc -l)
     echo "Port $PORT is used.."
 done
-``
 
 echo "Using Port $PORT.."
 
+./LinuxNoEditor/register.sh &
+
 echo "$UE4_PROJECT_ROOT/ShooterGame/Binaries/Linux/ShooterGameServer"
 chmod +x "$UE4_PROJECT_ROOT/ShooterGame/Binaries/Linux/ShooterGameServer"
-"$UE4_PROJECT_ROOT/ShooterGame/Binaries/Linux/ShooterGameServer" Highrise -server -log -nosteam PORT=$PORT $@ &
-
-echo $CHECK
-
-while [ "$CHECK" -eq "0" ]; do
-    CHECK=$(netstat -antp 2>/dev/null | grep $PORT | wc -l)
-    echo "Server is not ready yet.."
-    sleep 3
-done
-
-echo "Server is ready, sending register to DSM service.."
-
-response=$(curl -X POST --header "Content-Type: application/x-www-form-urlencoded" --header "Accept: application/json" --header "Authorization: Basic $BASIC_CREDS" -d "grant_type=client_credentials" "$BASE_URL/iam/oauth/token")
-token=$(echo "$response" | grep access_token | cut -d ':' -f 2 | cut -d '"' -f 2)
-curl -X POST --header "Content-Type: application/json" --header "Authorization: Bearer $token" -d "{ \"pod_name\": \"$POD_NAME\", \"port\": $PORT }" "$BASE_URL/dsm/namespaces/$namespace/servers/register"
+"$UE4_PROJECT_ROOT/ShooterGame/Binaries/Linux/ShooterGameServer" Highrise -server -log -nosteam PORT=$PORT $@
